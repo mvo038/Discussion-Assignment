1645b5b9f5aa127e1221574bbbddad3ca5dc5ea8
MervinxD <119443501+MervinxD@users.noreply.github.com>
2022-11-29 10:06:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,39 @@ def multi_sort(arr, cmp, method="None"):
         print("invalid argument!")
 # must be in-place sort
 def merge_sort(arr,cmp):
-    pass
+    
+if len(arr) > 1:
+ 
+    mid = len(arr)//2
+    L = arr[:mid]
+    R = arr[mid:]
+
+    mergeSort(L)
+    mergeSort(R)
+ 
+    i = 0
+    j = 0
+    k = 0
+        # Copy data to temp arrays L[] and R[]
+    while cmp(i, len(L)) < 0 and cmp(j, len(R)) < 0:
+        if cmp(L[i], R[j]) < 0:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+        # Checking if any element was left
+    while cmp(i, len(L)) < 0:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while cmp(j, len(R)) < 0:
+        arr[k] = R[j]
+        j += 1
+        k += 1
 # must be in-place sort
 def quick_sort(arr,cmp):
     pass
